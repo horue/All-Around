5046dac1c5177d56da5e9e22f673050ca6321c10
Jorge <jorgejapao00@gmail.com>
2024-06-06 20:32:52 -0300

    Character and camera updateds

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const speed = 5.0
 const JUMP_VELOCITY = 4.5
+
+
+@export var fall_acceleration = 75
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var target_velocity = Vector3.ZERO
+
 
 
 func _physics_process(delta):
@@ -19,3 +24,15 @@ func _physics_process(delta):
 		direction.x -= 1
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		
+
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+		# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+		# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
